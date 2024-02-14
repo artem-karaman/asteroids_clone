8dd4cb165be4a2754b354fb4ff2533e648336445
Artem Karaman <karaman55_09@mail.ru>
2024-02-14 10:45:28 +0600

    use input actions to handle player

--- AsteroidsClone/Assets/Code/View/Controller/PlayerController.cs
+++ AsteroidsClone/Assets/Code/View/Controller/PlayerController.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -7,15 +9,43 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private ScreenSettings _screenSettings;
 
+    [SerializeField]
+    private InputActionReference _moveAction;
+    [SerializeField]
+    private InputActionReference _shootAction;
+    [SerializeField]
+    private InputActionReference _rotateAction;
 
-    // Update is called once per frame
-    //void FixedUpdate()
-    // {
-    //    transform
-    //        .Translate(
-    //            _playerSettings.InstantSpeed * Time.fixedDeltaTime * Vector3.up
-    //        );
-    //}
+    private void OnEnable()
+    {
+        _moveAction.action.Enable();
+        _moveAction.action.performed += OnMoveAction;
+
+        _rotateAction.action.Enable();
+        _rotateAction.action.performed += OnRotateAction;
+    }
+
+    private void OnRotateAction(InputAction.CallbackContext context)
+    {
+        transform.Rotate(transform.position,
+            Vector3.Angle(transform.position,
+            context.ReadValue<Vector2>()));
+    }
+
+    private void OnMoveAction(InputAction.CallbackContext context)
+    {
+       transform
+            .Translate(
+                _playerSettings.InstantSpeed *
+                Time.fixedDeltaTime *
+                Vector2.up);
+    }
+
+    private void OnDisable()
+    {
+        _moveAction.action.Disable();
+        _rotateAction.action.Disable();
+    }
 
     void OnBecameInvisible()
     {
